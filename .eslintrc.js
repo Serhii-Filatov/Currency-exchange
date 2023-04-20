e9692f9f8cc23f9cc7337cdda89498e2097bf01c
Serhii Filatov <filser.spirit@gmail.com>
2023-04-20 18:13:37 +0300

    feat: added linters and pre-commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  
+  // * перенесенно с проекта calculator
+  plugins: ['prettier', 'import'],
+  ignorePatterns: ['.eslintrc.js', 'commitlint.config.js', 'package-lock.json'],
+  
+  // * перенесенно с проекта calculator
+  rules: {
+    'linebreak-style': ['error', (process.platform === 'win32' ? 'windows' : 'unix')],
+    'import/prefer-default-export': 'off',
+    'import/named': 'off',
+    'class-methods-use-this': 'off',
+    'import/no-cycle': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+  },
+};
